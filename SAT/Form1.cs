4caaed5f2563573faba740831f611187deb05c8f
Drazzild <drazzild@gmx.de>
2018-09-20 14:21:17 +0200

    Add files via upload

---
+++ SAT/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SAT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) 
+        {
+            button2.Enabled = false;
+        }
+
+        private void zahlenbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+
+        public int randomsec(int first, int second) //Random Number generator function
+        {
+            int first1 = (first * 1000);
+            int second1 = (second * 1000);
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(first1, second1);
+            return randomnumber;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //Timer Shit
+        {
+            timer1.Interval = randomsec(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+            SendKeys.Send(textBox1.Text + "{ENTER}");
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Start Button
+        {
+            if (textBox1.Text != "")
+            {
+                if(textBox2.Text != "")
+                {
+                    if (textBox3.Text != "")
+                    {
+                        if (Convert.ToInt32(textBox2.Text) > Convert.ToInt32(textBox3.Text))
+                        {
+                            MessageBox.Show("Zahl A darf nicht größer sein als Zahl B");
+                        }
+                        else
+                        {
+                            try
+                            {
+                                timer1.Interval = randomsec(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+                                label5.ForeColor = Color.Green;
+                                label5.Text = "Aktiviert";
+                                button1.Enabled = false;
+                                textBox1.Enabled = false;
+                                textBox2.Enabled = false;
+                                textBox3.Enabled = false;
+                                button2.Enabled = true;
+                                timer1.Enabled = true;
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Es dürfen nicht beide zahlen 0 sein");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Jo Zahl B darf nicht leer sein");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Jo Zahl A darf nicht leer sein");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Jo der text darf nicht leer sein");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // stop Button
+        {
+            label5.ForeColor = Color.Red;
+            label5.Text = "Deaktiviert";
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            button2.Enabled = false;
+            timer1.Enabled = false;
+        }
+    }
+}
---
+++ SAT/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("SAT")]
+[assembly: AssemblyDescription("Simple Auto Typer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SAT")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("c29bed7e-14b7-450f-b98d-4137fbb80e76")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
